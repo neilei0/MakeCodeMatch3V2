6fde34f8e7c2510b605f8f8536a4ee8f030776f3
neilei0 <74994441+neilei0@users.noreply.github.com>
2024-04-29 22:48:56 -0700

    Added swapping and single layer of recursion for match checking.

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+namespace SpriteKind {
+    export const tile1 = SpriteKind.create()
+    export const tile2 = SpriteKind.create()
+    export const tile3 = SpriteKind.create()
+    export const tile4 = SpriteKind.create()
+    export const tile5 = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (selectMode == -1) {
         grid.place(cursor, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) - 1))
@@ -6,6 +13,9 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (selectMode == 1) {
+        grid.place(cursor, tiles.getTileLocation(loc1, loc2))
+    }
     selectMode = -1
     cursor.setImage(img`
         f f f f f f f f f f f f f f f f 
@@ -51,6 +61,9 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         ep1 = grid.getSprites(tiles.getTileLocation(loc1, loc2))[0]
         ep2 = grid.getSprites(grid.getLocation(cursor))[0]
         grid.swap(ep1, ep2)
+        for (let i of grid.allSprites()) {
+            matchCheck(i, 1)
+        }
     } else {
         selectMode = 1
         music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
@@ -99,7 +112,7 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function generate (num: number, num2: number) {
     list = [
-    img`
+    sprites.create(img`
         . f f . . . . . . . . . . f . . 
         f f 4 f . . . . . . . . f f f . 
         f 4 4 f . . . . . . . . f 4 f f 
@@ -116,8 +129,8 @@ function generate (num: number, num2: number) {
         . f f f 5 4 4 4 4 4 4 5 5 4 f . 
         . . . f f f f f f f f f f f . . 
         . . . . f 8 8 8 8 8 c f . . . . 
-        `,
-    img`
+        `, SpriteKind.tile1),
+    sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . f f f f f f f f f f f f . . 
         . f f 5 5 5 5 5 5 5 5 5 5 f f . 
@@ -134,8 +147,8 @@ function generate (num: number, num2: number) {
         . . . f 5 5 5 5 5 5 5 e f f . . 
         . . . . f f f f f f f f e . . . 
         . . . . . . . . . . . . . . . . 
-        `,
-    img`
+        `, SpriteKind.tile2),
+    sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -152,8 +165,8 @@ function generate (num: number, num2: number) {
         . . . . f f 3 f f 3 f f . . . . 
         . . . f f 3 f 3 3 f f . . . . . 
         . . . f 3 3 3 3 f . . . . . . . 
-        `,
-    img`
+        `, SpriteKind.tile3),
+    sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . f f f . . . . . f f f f . . 
         . f 4 4 4 f f f f f 4 4 4 f . . 
@@ -170,8 +183,8 @@ function generate (num: number, num2: number) {
         . . . . f f f f f f f . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `,
-    img`
+        `, SpriteKind.tile4),
+    sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . f f f f f f f f f f f . . 
         . . f b b d d d d d d b b f f . 
@@ -188,16 +201,58 @@ function generate (num: number, num2: number) {
         f b b f . f f f f f f f b b f . 
         . f b f . . . . . . . . f f f . 
         . f f f . . . . . . . . f f . . 
-        `
+        `, SpriteKind.tile5)
     ]
-    tile = sprites.create(list._pickRandom(), SpriteKind.Food)
+    tile = list._pickRandom()
     grid.place(tile, tiles.getTileLocation(num, num2))
 }
-function matchCheck () {
-	
+function matchCheck (checkSprite: Sprite, num: number) {
+    threeFound = 0
+    count = num
+    checks = [
+    grid.lineAdjacentSprites(grid.getLocation(checkSprite), CollisionDirection.Left),
+    grid.lineAdjacentSprites(grid.getLocation(checkSprite), CollisionDirection.Top),
+    grid.lineAdjacentSprites(grid.getLocation(checkSprite), CollisionDirection.Right),
+    grid.lineAdjacentSprites(grid.getLocation(checkSprite), CollisionDirection.Bottom)
+    ]
+    ch2 = []
+    for (let i of checks) {
+        for (let j of i) {
+            ch2.push(j)
+        }
+    }
+    for (let value of ch2) {
+        if (checkSprite.kind() == value.kind()) {
+            count += 1
+            goo = sprites.create(img`
+                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 . . . . . . . . . . . . . . 7 
+                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                `, SpriteKind.Player)
+            grid.place(goo, grid.getLocation(checkSprite))
+        }
+    }
 }
+let goo: Sprite = null
+let ch2: Sprite[] = []
+let checks: Sprite[][] = []
+let count = 0
+let threeFound = 0
 let tile: Sprite = null
-let list: Image[] = []
+let list: Sprite[] = []
 let ep2: Sprite = null
 let ep1: Sprite = null
 let loc2 = 0
